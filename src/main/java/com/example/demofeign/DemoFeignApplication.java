bc73710bf4f09957970cba47ace6dbf23bd52752
menuHwang <hmin0206@naver.com>
2023-10-04 15:30:28 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/demofeign/DemoFeignApplication.java
@@ -0,0 +1,13 @@
+package com.example.demofeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFeignApplication.class, args);
+    }
+
+}
